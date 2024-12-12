14c06bf3f62b09704f2330d791b019b4e0b068c7
Martins <victorcostamartins0@gmail.com>
2024-12-12 14:57:29 -0300

    To-do List

--- script.js
+++ script.js
@@ -2,40 +2,7 @@ const container = document.querySelector(".container");
 const boxItens = document.querySelector(".box-itens")
 const frm = document.querySelector("form");
 
-const lista = []
-
-frm.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    const item = frm.inItem.value;
-    
-    if(item == "") {
-        alert("Pfvr, adicione um item para continuarmos!")
-        frm.inItem.focus();
-        return
-    }
-    lista.push(item);
-
-    elementTemp(item)
-    frm.reset()
-    frm.inItem.focus()
-})
-
 const elementTemp = (item) => {
-    `
-    <li class="item">
-                <div class="item-cont">
-                    <p></p>
-                </div>
-
-                <div class="butns">
-                    <button class="btn" id="btEdit"> <i class="bi bi-check"></i> 
-                    </button>
-                    <button class="btn" id="btDelet"> <i class="bi bi-trash3"></i>
-                    </button>
-                </div>
-            </li>
-    `
 
     const li = document.createElement("li");
     li.classList.add("item")
@@ -52,13 +19,19 @@ const elementTemp = (item) => {
     divButns.classList.add("butns")
     li.appendChild(divButns)
 
-    const btEdit = document.createElement("button");
-    const itemEdd = document.createElement("i")
-    btEdit.classList.add("btn")
-    itemEdd.classList.add("bi-check")
-    divButns.appendChild(btEdit)
-    btEdit.appendChild(itemEdd)
+    const btCheck = document.createElement("button");
+    const itemCheck = document.createElement("i")
+    btCheck.classList.add("btn")
+    itemCheck.classList.add("bi-check") 
+    divButns.appendChild(btCheck)
+    btCheck.appendChild(itemCheck)
     
+    const btEdit = document.createElement("button");
+    const itemEdit = document.createElement("i")
+    btEdit.classList.add("btn")
+    itemEdit.classList.add("bi-pencil-fill") 
+    divButns.appendChild(btEdit)
+    btEdit.appendChild(itemEdit)
 
     const btDelet = document.createElement("button");
     const itemDelt = document.createElement("i")
@@ -67,14 +40,38 @@ const elementTemp = (item) => {
     divButns.appendChild(btDelet)
     btDelet.appendChild(itemDelt)
 
+    btCheck.addEventListener("click", () => {
+        li.classList.toggle("hidden")
+    });
+
     btEdit.addEventListener("click", () => {
-        let itemText = item
-    })
+        const eddText = prompt(`Editar item` )
+
+        item.innerHtml = eddText
+    });
 
     btDelet.addEventListener("click", (e) => {
-        e.target;
-        console.log(e)
-        
+        const targtElemt = e.target;
+        const parentElemt = targtElemt.closest("li")
+
+        parentElemt.remove()
     })
 
 }
+
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const item = frm.inItem.value;
+    
+    if(item == "") {
+        alert("Pfvr, adicione um item para continuarmos!")
+        frm.inItem.focus();
+        return
+    }
+
+    elementTemp(item)
+    frm.reset()
+    frm.inItem.focus()  
+})
